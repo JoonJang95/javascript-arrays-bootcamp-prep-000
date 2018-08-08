b2e6cb0758bf14fbc23c2cc232d9acc9d9a8bdf4
Joon Jang <joon.jang95@gmail.com>
2018-08-08 02:59:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,42 @@
-var chocolateBars = Array('snickers','hundred grand','kitkat','skittles');
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray([a],foo) {
-  return ['foo',...1]
+function addElementToBeginningOfArray(Array, element) {
+    Array = [element, ...Array];
+    return Array 
+} 
+
+function destructivelyAddElementToBeginningOfArray(Array, element) {
+  Array.unshift(element);
+   return Array
+}
+
+
+function addElementToEndOfArray(array, element) {
+   array = [...array, element];
+   return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array
+}
+
+function accessElementInArray(array,index) {
+  return array[index];
+  }
+  
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array 
+}
+
+function removeElementFromEndOfArray(bae) {
+  bae.slice()
+  return bae
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array2 = array.slice(1); 
+  return array2
 }
 
